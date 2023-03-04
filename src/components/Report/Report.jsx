61d730dcd74e678bf4269ed4e60a78f11b8f03ef
ishankmoraiya <ishankmoraiya196@gmail.com>
2023-03-04 10:45:54 +0530

    change in logo

--- src/components/Report/Report.jsx
+++ src/components/Report/Report.jsx
@@ -1,8 +1,18 @@
-// import React from "react";
+import React from "react";
 // import { Link } from "react-router-dom";
 import "./Report.scss";
 
 function Report() {
-  return ;
+  return (
+    <div classname="report-div">
+      <div className="firstdiv">
+          
+       
+      </div>
+      <div className="secdiv">
+        <h1>hello</h1>
+      </div>
+    </div>
+  );
 }
 export default Report;
